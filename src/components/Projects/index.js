64ebde4eb226298a5fa55e984c1a2398e8251224
Mary Braunger <maryjezek@aol.com>
2022-04-09 12:41:55 -0400

    utilizing react

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,42 +6,20 @@ import image1 from "../../assets/small/projects/1.gif";
 import image2 from "../../assets/small/projects/2.gif";
 import image3 from "../../assets/small/projects/3.gif";
 import image4 from "../../assets/small/projects/4.gif";
+import Project from "./Project"
+
+const PROJECTS =  [ {heading:"Fast n Furious Garage", url:"https://fast-n-furious.herokuapp.com/", image:image0, alt:"fastnfurious"},
+{heading:"Travel Optimizer", url:"https://maryjezek.github.io/travel-optimizer/", image:image1, alt:"optimizer"},
+//{heading:"Fast n Furious Garage", url:"https://fast-n-furious.herokuapp.com/", image:image0, alt:"fastnfurious"},
+//{heading:"Fast n Furious Garage", url:"https://fast-n-furious.herokuapp.com/", image:image0, alt:"fastnfurious"},
+//<Project heading="Fast n Furious Garage" url="https://fast-n-furious.herokuapp.com/" image={image0} alt="fastnfurious"  />
+]
 
 function Projects(props) {
   const { currentCategory } = props;
   return (
-    <>
-      <>
-        <>
-          <section class="card">
-            <h1>Fast n Furious Garage</h1>
-            <a href="https://fast-n-furious.herokuapp.com/">
-              {" "}
-              <img
-                src={image0}
-                alt="fastnfurious"
-                className="projectImg"
-              />{" "}
-            </a>
-            <p>
-              <a href="https://fast-n-furious.herokuapp.com/">
-                Fast n Furious Garage
-              </a>
-            </p>
-          </section>
-          <section class="runBuddySection card">
-            <h1>Travel Optimizer</h1>
-            <a href="https://maryjezek.github.io/travel-optimizer/">
-              {" "}
-              <img src={image1} alt="optimizer" class="runBuddy" />{" "}
-            </a>
-            <p>
-              <a href="https://maryjezek.github.io/travel-optimizer/">
-                Travel Optimizer
-              </a>
-            </p>
-          </section>
-        </>
+    <div style={{display:"flex"}}>
+   {PROJECTS.map(proj => <Project heading={proj.heading} url={proj.url} image={proj.image} alt={proj.alt}  />)}
         <section class="card">
           <h1>Weather Dashboard</h1>
           <a href="https://maryjezek.github.io/weather_dashboard/">
@@ -54,7 +32,7 @@ function Projects(props) {
             </a>
           </p>
         </section>
-      </>
+      
       <section class="card">
         <h1>Password Generator</h1>
         <a href="https://maryjezek.github.io/password_generator/">
@@ -79,7 +57,7 @@ function Projects(props) {
           </a>
         </p>
       </section>
-    </>
+     </div>
   );
 }
 export default Projects;
