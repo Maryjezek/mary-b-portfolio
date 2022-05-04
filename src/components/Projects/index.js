931bfd5fa4a560c82a1d5288d08c35b3eae1014f
Mary Braunger <maryjezek@aol.com>
2022-05-03 21:47:26 -0400

    adding new project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,33 +6,59 @@ import image1 from "../../assets/small/projects/1.gif";
 import image2 from "../../assets/small/projects/2.gif";
 import image3 from "../../assets/small/projects/3.gif";
 import image4 from "../../assets/small/projects/4.gif";
-import Project from "./Project"
+import image5 from "../../assets/small/projects/5.png";
+import Project from "./Project";
 
-const PROJECTS =  [ {heading:"Fast n Furious Garage", url:"https://fast-n-furious.herokuapp.com/", image:image0, alt:"fastnfurious"},
-{heading:"Travel Optimizer", url:"https://maryjezek.github.io/travel-optimizer/", image:image1, alt:"optimizer"},
-//{heading:"Fast n Furious Garage", url:"https://fast-n-furious.herokuapp.com/", image:image0, alt:"fastnfurious"},
-//{heading:"Fast n Furious Garage", url:"https://fast-n-furious.herokuapp.com/", image:image0, alt:"fastnfurious"},
-//<Project heading="Fast n Furious Garage" url="https://fast-n-furious.herokuapp.com/" image={image0} alt="fastnfurious"  />
-]
+const PROJECTS = [
+  {
+    heading: "Silver Socials",
+    url: "https://silver-socials.herokuapp.com/",
+    image: image5,
+    alt: "SilverSocials",
+  },
+  {
+    heading: "Fast n Furious Garage",
+    url: "https://fast-n-furious.herokuapp.com/",
+    image: image0,
+    alt: "fastnfurious",
+  },
+  {
+    heading: "Travel Optimizer",
+    url: "https://maryjezek.github.io/travel-optimizer/",
+    image: image1,
+    alt: "optimizer",
+  },
+
+  //{heading:"Fast n Furious Garage", url:"https://fast-n-furious.herokuapp.com/", image:image0, alt:"fastnfurious"},
+  //{heading:"Fast n Furious Garage", url:"https://fast-n-furious.herokuapp.com/", image:image0, alt:"fastnfurious"},
+  //<Project heading="Fast n Furious Garage" url="https://fast-n-furious.herokuapp.com/" image={image0} alt="fastnfurious"  />
+];
 
 function Projects(props) {
   const { currentCategory } = props;
   return (
-    <div style={{display:"flex"}}>
-   {PROJECTS.map(proj => <Project heading={proj.heading} url={proj.url} image={proj.image} alt={proj.alt}  />)}
-        <section class="card">
-          <h1>Weather Dashboard</h1>
+    <div style={{ display: "flex" }}>
+      {PROJECTS.map((proj) => (
+        <Project
+          heading={proj.heading}
+          url={proj.url}
+          image={proj.image}
+          alt={proj.alt}
+        />
+      ))}
+      <section class="card">
+        <h1>Weather Dashboard</h1>
+        <a href="https://maryjezek.github.io/weather_dashboard/">
+          {" "}
+          <img src={image2} alt="Weather" class="projectImg" />{" "}
+        </a>
+        <p>
           <a href="https://maryjezek.github.io/weather_dashboard/">
-            {" "}
-            <img src={image2} alt="Weather" class="projectImg" />{" "}
+            Weather Dashboard
           </a>
-          <p>
-            <a href="https://maryjezek.github.io/weather_dashboard/">
-              Weather Dashboard
-            </a>
-          </p>
-        </section>
-      
+        </p>
+      </section>
+
       <section class="card">
         <h1>Password Generator</h1>
         <a href="https://maryjezek.github.io/password_generator/">
@@ -57,7 +83,7 @@ function Projects(props) {
           </a>
         </p>
       </section>
-     </div>
+    </div>
   );
 }
 export default Projects;
